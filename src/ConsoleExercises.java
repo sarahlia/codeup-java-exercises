50f9b890f9bdc2c242abfd729bc355bbb9515237
Sarah L Nainggolan <sarah.l.nainggolan@gmail.com>
2020-05-01 15:46:55 -0500

    finish console exercise including bonus after walkthrough

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -4,14 +4,15 @@ import static java.lang.Integer.parseInt;
 
 public class ConsoleExercises {
     public static void main(String args[]) {
-        double pi = 3.14159;
-        System.out.printf("The value of pi is approximately %,.2f.%n", pi);
-
         Scanner scanner = new Scanner(System.in);
+        double pi = 3.14159;
+        System.out.printf("The value of pi is approximately %.2f%n", pi);
+
 //        System.out.println("Enter an integer");
 //        int anInteger = scanner.nextInt();
 //
-//        System.out.printf("You entered %,d%n", anInteger);
+//        System.out.println("You entered " + anInteger);
+
 
 //        System.out.println("Enter 3 words, each on a separate line.");
 //        String wordOne = scanner.nextLine();
@@ -21,34 +22,23 @@ public class ConsoleExercises {
 //        System.out.printf("You like %s, hate %s, and are neutral about %s.", wordOne, wordTwo, wordThree);
 
 //        System.out.println("Enter a sentence.");
-////        String sentence = scanner.next();
+//        String sentence = scanner.next();//this is just for show-and-tell. will only output the first word.
 //        String sentence = scanner.nextLine();
 
 //        System.out.printf("%s", sentence);
 
         System.out.println("Enter length:");
-        String aLength = scanner.nextLine();
-        int length = Integer.parseInt(aLength);
-        System.out.println(length);
+        float aLength = Float.parseFloat(scanner.nextLine());
+        System.out.println(aLength);
 
         System.out.println("Enter width:");
-        String aWidth = scanner.nextLine();
+        float aWidth = Float.parseFloat(scanner.nextLine());
 
-        int width = Integer.parseInt(aWidth);
-        System.out.println(width);
+        float perimeter = 2 * (aLength + aWidth);
+        System.out.println("The perimeter is " + perimeter);
 
-        int perimeter = 2 * (length + width);
-        System.out.printf("The perimeter is %,d%n", perimeter);
-
-        int area = length * width;
-        System.out.printf("The area is %,d", area);
-
-
-
-
-
-
-
+        float area = aLength * aWidth;
+        System.out.println("The area is " + area);
 
 
     }
