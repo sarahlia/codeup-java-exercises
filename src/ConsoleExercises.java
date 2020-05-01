f305c71cd00d02f85256d50f1303fc2097f8bcd6
Sarah L Nainggolan <sarah.l.nainggolan@gmail.com>
2020-05-01 14:56:10 -0500

    finish console IO exercises

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 
+import static java.lang.Integer.parseInt;
+
 public class ConsoleExercises {
     public static void main(String args[]) {
         double pi = 3.14159;
@@ -18,11 +20,33 @@ public class ConsoleExercises {
 //
 //        System.out.printf("You like %s, hate %s, and are neutral about %s.", wordOne, wordTwo, wordThree);
 
-        System.out.println("Enter a sentence.");
-//        String sentence = scanner.next();
-        String sentence = scanner.nextLine();
+//        System.out.println("Enter a sentence.");
+////        String sentence = scanner.next();
+//        String sentence = scanner.nextLine();
 
-        System.out.printf("%s", sentence);
+//        System.out.printf("%s", sentence);
+
+        System.out.println("Enter length:");
+        String aLength = scanner.nextLine();
+        int length = Integer.parseInt(aLength);
+        System.out.println(length);
+
+        System.out.println("Enter width:");
+        String aWidth = scanner.nextLine();
+
+        int width = Integer.parseInt(aWidth);
+        System.out.println(width);
+
+        int perimeter = 2 * (length + width);
+        System.out.printf("The perimeter is %,d%n", perimeter);
+
+        int area = length * width;
+        System.out.printf("The area is %,d", area);
+
+
+
+
+
 
 
 
