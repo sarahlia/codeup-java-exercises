58d14dca97f17de2f115dbe6abf6899fdb97cf66
Sarah L Nainggolan <sarah.l.nainggolan@gmail.com>
2020-05-04 12:07:40 -0500

    finish exercise 1 using for loop.

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,28 +1,46 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        int i = 5;
-        while(i <= 15) {
-            System.out.println("From a while loop. i = " + i);
-            i++;
+//        int i = 5;
+//        while(i <= 15) {
+//            System.out.println("From a while loop. i = " + i);
+//            i++;
+//        }
+//
+//        int counter = 0;
+//        do {
+//            System.out.println("From a do-while loop. counter = " + counter);
+//            counter += 2;
+//        } while (counter <= 100);
+//
+//        int x = 100;
+//        do {
+//            System.out.println("Do-while loop, counting backwards from 100. x = " + x);
+//            x -= 5;
+//        } while (x >= -10);
+//
+//        int y = 1;
+//        do {
+//            System.out.println("Do-while loop. " + (int) Math.pow(2, y));
+//            y *= 2;
+//        } while (y <= 16);
+
+        //Using for loop instead:
+        for(int i = 5; i <= 15; i++) {
+            System.out.println("From a for loop. i = " + i);
         }
 
-        int counter = 0;
-        do {
-            System.out.println("From a do-while loop. counter = " + counter);
-            counter += 2;
-        } while (counter <= 100);
+        for(int counter = 0; counter <= 100; counter += 2) {
+            System.out.println("From a for loop, counting up to 100 by 2s. " + counter);
+        }
 
-        int x = 100;
-        do {
-            System.out.println("Do-while loop, counting backwards from 100. x = " + x);
-            x -= 5;
-        } while (x >= -10);
+        for(int x = 100; x >= -10; x -=5) {
+            System.out.println("For loop, counting backwards from 100 by 5s. " + x);
+        }
 
-        int y = 1;
-        do {
-            System.out.println("Do-while loop. " + Math.pow(2, y));
-            y *= 2;
-        } while (y <= 16);
+        for(int y = 1; y <= 16; y *=2) {
+            int intResult = (int) Math.pow(2,y);
+            System.out.println("Squares using for loop. " + intResult);
+        }
 
     }
 }
