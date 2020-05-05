7c01a9a3f3c954bc0b24de7abf4b8e03f4225f19
Sarah L Nainggolan <sarah.l.nainggolan@gmail.com>
2020-05-05 15:35:35 -0500

    finish practice after Methods Lecture

--- src/MethodsLec.java
+++ src/MethodsLec.java
@@ -3,6 +3,8 @@ public class MethodsLec {
         public static void main(String[] args) {
             System.out.println(sayHello("Sarah"));
             System.out.println(sum(5, 12));
+            System.out.println(sum(2.5, 8.5));
+            System.out.println(sayHello("Sarah", "Nainggolan"));
         }
 
         public static String sayHello(String name) {
@@ -12,6 +14,14 @@ public class MethodsLec {
 
         public static int sum(int num1, int num2) {
             return num1 + num2;
+        }
+
+        public static double sum(double num1, double num2) {
+            return num1 + num2;
+        }
+
+        public static String sayHello(String firstName, String lastName) {
+            return "Hello, " + firstName + lastName;
         }
 
 
