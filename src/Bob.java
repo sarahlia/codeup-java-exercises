9fdbd47b1fdb16e50f22c2d7140ef989e0647e20
Sarah L Nainggolan <sarah.l.nainggolan@gmail.com>
2020-05-05 11:52:28 -0500

    finish Bob string exercise after walkthrough

--- src/Bob.java
+++ src/Bob.java
@@ -5,20 +5,35 @@ public class Bob {
 
           Scanner sc = new Scanner(System.in);
 
-          for(int i = 0; i <= 10; i++) {
-             System.out.println("Say something to Bob:");
-             String userInput = sc.nextLine();
-             if (userInput.endsWith("?")) {
-                 System.out.println("Sure.");
-             } else if (userInput.endsWith("!")) {
-                 System.out.println("Whoa, chill out!");
-             } else if (userInput.equals("")) {
-                 System.out.println("Fine. Be that way!");
-                 break;
-             } else {
-                 System.out.println("Whatever.");
-             }
-          }
+//          for(int i = 0; i <= 10; i++) {
+//             System.out.println("Say something to Bob:");
+//             String userInput = sc.nextLine();
+//             if (userInput.endsWith("?")) {
+//                 System.out.println("Sure.");
+//             } else if (userInput.endsWith("!")) {
+//                 System.out.println("Whoa, chill out!");
+//             } else if (userInput.equals("")) {
+//                 System.out.println("Fine. Be that way!");
+//                 break; // if we want to break out of the loop when Bob is upset and says "Be that way!"
+//             } else {
+//                 System.out.println("Whatever.");
+//             }
+//          }
+
+         //DOUGLAS' WAY:
+//         System.out.println("What will you say to Bob! ");
+//         String userResponse = sc.nextLine().trim();
+//         System.out.println(userResponse);
+//
+//         if(userResponse.endsWith("?")) {
+//             System.out.println("Sure.");
+//         } else if (userResponse.endsWith("!")) {
+//             System.out.println("Whoa, chill out!");
+//         } else if (userResponse.equals(" ")) {
+//             System.out.println("Fine. Be that way!");
+//         } else {
+//             System.out.println("Whatever.");
+//         }
 
      }
 }
