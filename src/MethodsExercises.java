21e56c3e76fec1946c852dacb09b35db717f0c2a
Sarah L Nainggolan <sarah.l.nainggolan@gmail.com>
2020-05-05 23:35:09 -0500

    finish exercise 1 with bonuses

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -2,7 +2,8 @@ public class MethodsExercises {
     public static void main(String[] args) {
         System.out.println("add(3, 5) = " + add(3, 5));
         System.out.println("subtract(10, 7) = " + subtract(10, 7));
-        System.out.println("multiply(4, 8) = " + multiply(4, 8));
+        System.out.println("multiply(6, 4) = " + multiply(6, 4));
+        System.out.println("multiply(4, -3) = " + multiply(4, -3));
         System.out.println("divide(69, 23) = " + divide(69, 23));
         System.out.println("modulus(13, 4) = " + modulus(13, 4));
     }
@@ -15,9 +16,9 @@ public class MethodsExercises {
         return num1 - num2;
     }
 
-    public static int multiply(int num1, int num2) {
-        return num1 * num2;
-    }
+//    public static int multiply(int num1, int num2) {
+//        return num1 * num2;
+//    }
 
     public static int divide(int num1, int num2) {
         return num1 / num2;
@@ -26,4 +27,28 @@ public class MethodsExercises {
     public static int modulus(int num1, int num2) {
         return num1 % num2;
     }
+
+    //exercise 1 bonus using for loop
+//    public static int multiply(int num1, int num2) {
+//        int result = 1;
+//        for(int i = 1; i <= num2; i++) {
+//            result = result + num1;
+//        }
+//        return result - 1;
+//    }
+
+    //exercise 1 bonus using recursion
+    public static int multiply(int num1, int num2) {
+        if (num1 == 0 || num2 ==0 ) {
+            return 0;
+        } else if (num2 > 0) {
+            return num1 + multiply(num1, num2 - 1);
+//        }
+        } else {
+            return -num1 + multiply(num1, num2 + 1);
+        }
+    }
+
+
+
 }
