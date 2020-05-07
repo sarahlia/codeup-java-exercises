53c59e84bbad038e9c886a3164c1b6a39134d03f
Sarah L Nainggolan <sarah.l.nainggolan@gmail.com>
2020-05-07 15:55:10 -0500

    finish exercise 2

--- src/Person.java
+++ src/Person.java
@@ -23,6 +23,23 @@ public class Person {
         p1.setName("fernando");
         p1.sayHello();
 
+//        Person person1 = new Person("John");
+//        Person person2 = new Person("John");
+//        System.out.println(person1.getName().equals(person2.getName()));
+//        System.out.println(person1 == person2);
+
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1 == person2);
+
+        Person person1 = new Person("John");
+        Person person2 = person1;
+        System.out.println(person1.getName());
+        System.out.println(person2.getName());
+        person2.setName("Jane");
+        System.out.println(person1.getName());
+        System.out.println(person2.getName());
+
     }
 
 }
