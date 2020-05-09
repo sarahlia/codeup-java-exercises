29c4b0047605ba7d0094949c7f11ba3474f2216a
Sarah L Nainggolan <sarah.l.nainggolan@gmail.com>
2020-05-09 00:13:19 -0500

    TODO: finish exercise2

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -47,4 +47,7 @@ public class ArraysExercises {
         return result;
     }
 
+
+
+
 }
---
+++ src/ServerNameGenerator.java
@@ -0,0 +1,15 @@
+public class ServerNameGenerator {
+
+
+
+    String[] adjectives = {"lazy", "sleepy", "grouchy", "handsome", "beautiful", "stinky", "happy", "sad", "shy", "arrogant"};
+    String[] nouns = {"lion", "tiger", "bear", "pig", "sister", "brother", "mother", "father", "teacher", "student"};
+
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
+
