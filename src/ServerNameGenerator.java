d30ba94454fe297fa82c8f75c74b08099863333a
Sarah L Nainggolan <sarah.l.nainggolan@gmail.com>
2020-05-09 22:57:44 -0500

    finish exercise 2

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -1,13 +1,26 @@
+import java.util.Random;
+
 public class ServerNameGenerator {
-
-
 
     String[] adjectives = {"lazy", "sleepy", "grouchy", "handsome", "beautiful", "stinky", "happy", "sad", "shy", "arrogant"};
     String[] nouns = {"lion", "tiger", "bear", "pig", "sister", "brother", "mother", "father", "teacher", "student"};
 
+    Random random = new Random();
+
+    public String getRandomAdjective() {
+        int indexAdj = random.nextInt(adjectives.length);
+        return adjectives[indexAdj];
+    }
+
+    public String getRandomNoun() {
+        int indexNoun = random.nextInt(nouns.length);
+        return nouns[indexNoun];
+    }
 
     public static void main(String[] args) {
-
+        System.out.println("Here is your server name: ");
+        ServerNameGenerator s1 = new ServerNameGenerator();
+        System.out.println(s1.getRandomAdjective() + "-" + s1.getRandomNoun());
     }
 
 
