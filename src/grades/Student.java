fdd40930dbe682a3ff2e434ef1f600eb4ffa17e0
Sarah L Nainggolan <sarah.l.nainggolan@gmail.com>
2020-05-13 15:02:44 -0500

    finish collections exercise 1

---
+++ src/grades/Student.java
@@ -0,0 +1,49 @@
+package grades;
+
+import java.util.ArrayList;
+
+public class Student {
+    private String name;
+    private ArrayList<Integer> grades;
+
+    public Student(String name) {
+        this.name = name;
+        this.grades = new ArrayList<>();
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void addGrade(int grade) {
+        this.grades.add(grade);
+    }
+
+    public double getGradeAverage() {
+
+        double totalGrade = 0;
+        for (double grade : this.grades) {
+            totalGrade = totalGrade + grade;
+        }
+        return totalGrade / this.grades.size();
+
+//        For-loop method:
+//        double totalGrade = 0;
+//        for(int i = 0; i < grades.size(); i++) {
+//            totalGrade = totalGrade + grades.get(i);
+//        }
+//        return totalGrade / grades.size();
+    }
+
+    public static void main(String[] args) {
+        Student joanne = new Student("joanne");
+        joanne.addGrade(95);
+        joanne.addGrade(90);
+        joanne.addGrade(100);
+
+        System.out.println("joanne.grades = " + joanne.grades);
+        System.out.println("joanne.getGradeAverage() = " + joanne.getGradeAverage());
+
+    }
+
+}
