c327093a7f77705b579e7ae026c16d7ebae86d7c
Sarah L Nainggolan <sarah.l.nainggolan@gmail.com>
2020-06-09 21:03:35 -0500

    finish FizzBuzz exercise using Java

--- src/homepractice/Mockprep.java
+++ src/homepractice/Mockprep.java
@@ -12,6 +12,18 @@ public class Mockprep {
         anArray.add("c");
         System.out.println(anArray);
 
+        //Fizz/Buzz: numbers divisible by 3: Fizz, numbers divisible by 5: Buzz, numbers divisible by both 3 AND 5: FizzBuzz
+        for(int i=1; i<=100; i++) {
+            if(i % 15 == 0) {
+                System.out.println("FizzBuzz");
+            } else if(i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if(i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
 
     }
 }
