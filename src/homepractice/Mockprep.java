2f0220df227f8f2dd6acf3678f8fce0873d963b1
Sarah L Nainggolan <sarah.l.nainggolan@gmail.com>
2020-06-09 20:20:08 -0500

    create an ArrayList of Strings with 3 string objects

---
+++ src/homepractice/Mockprep.java
@@ -0,0 +1,17 @@
+package homepractice;
+
+import java.util.ArrayList;
+
+public class Mockprep {
+    public static void main(String[] args) {
+
+        //Make an ArrayList of Strings with 3 string objects
+        ArrayList<String> anArray = new ArrayList<>();
+        anArray.add("a");
+        anArray.add("b");
+        anArray.add("c");
+        System.out.println(anArray);
+
+
+    }
+}
