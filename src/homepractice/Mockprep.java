464314a0e486ffa025a2886d43aedf2363c8c7db
Sarah L Nainggolan <sarah.l.nainggolan@gmail.com>
2020-06-14 22:59:51 -0500

    add some examples of HashMap methods

--- src/homepractice/Mockprep.java
+++ src/homepractice/Mockprep.java
@@ -1,36 +1,57 @@
 package homepractice;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Mockprep {
     public static void main(String[] args) {
 
         //Make an ArrayList of Strings with 3 string objects
-        ArrayList<String> anArray = new ArrayList<>();
-        anArray.add("a");
-        anArray.add("b");
-        anArray.add("c");
-        System.out.println(anArray);
+//        ArrayList<String> anArray = new ArrayList<>();
+//        anArray.add("a");
+//        anArray.add("b");
+//        anArray.add("c");
+//        System.out.println(anArray);
 
         //Fizz/Buzz: numbers divisible by 3: Fizz, numbers divisible by 5: Buzz, numbers divisible by both 3 AND 5: FizzBuzz
-        for(int i=1; i<=100; i++) {
-            if(i % 15 == 0) {
-                System.out.println("FizzBuzz");
-            } else if(i % 3 == 0) {
-                System.out.println("Fizz");
-            } else if(i % 5 == 0) {
-                System.out.println("Buzz");
-            } else {
-                System.out.println(i);
-            }
-        }
+//        for(int i=1; i<=100; i++) {
+//            if(i % 15 == 0) {
+//                System.out.println("FizzBuzz");
+//            } else if(i % 3 == 0) {
+//                System.out.println("Fizz");
+//            } else if(i % 5 == 0) {
+//                System.out.println("Buzz");
+//            } else {
+//                System.out.println(i);
+//            }
+//        }
 
         //call countToThirtyFive method
-        countToThirtyFive();
+//        countToThirtyFive();
 
         //call countTo method
-        countTo(17);
+//        countTo(17);
 
+
+        // We'll start by defining a hash map
+        HashMap<String, Integer> students = new HashMap<>();
+
+// and putting some data into it
+        students.put("Ryan", 1);
+        students.put("Zach", 5);
+        students.put("Fernando", 2);
+        students.put("Justin", 19);
+
+        System.out.println(students);
+
+// obtaining values from the hash map by key
+        System.out.println("students.get(\"Ryan\") = " + students.get("Ryan"));
+        System.out.println("students.get(\"Phillip\") = " + students.get("Phillip"));
+        System.out.println("students.getOrDefault(\"Jason\", 0) = " + students.getOrDefault("Jason", 0));
+
+// checking if keys or values are present
+        System.out.println("students.containsKey(\"Justin\") = " + students.containsKey("Justin"));
+        System.out.println("students.containsValue(4) = " + students.containsValue(4));
     }
 
     //Write a Java for loop method that prints out 0 to 35 when called
@@ -45,6 +66,8 @@ public class Mockprep {
         for(int i = 0; i <= param; i++) {
             System.out.println(i);
         }
+
+
     }
 
 
