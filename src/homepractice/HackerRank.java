27ac23419c1e51eeb8bd129c08040f2daccd5ec7
Sarah L Nainggolan <sarah.l.nainggolan@gmail.com>
2020-08-03 16:27:41 -0500

    finish anagram exercise.

--- src/homepractice/HackerRank.java
+++ src/homepractice/HackerRank.java
@@ -2,6 +2,7 @@ package homepractice;
 
 import java.sql.SQLOutput;
 import java.text.NumberFormat;
+import java.util.HashMap;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -142,25 +143,80 @@ public class HackerRank {
 //        System.out.print(my_calculator.divisor_sum(n) + "\n");
 //        sc.close();
 
-        String A = "hello";
-        String B = "java";
+//        String A = "hello";
+//        String B = "java";
+//
+//        System.out.println(capitalizeString(A) + " " + capitalizeString(B));
+//        System.out.println(A.compareTo(B) > 0 ? "Yes" : "No");
 
-        System.out.println(capitalizeString(A) + " " + capitalizeString(B));
-        System.out.println(A.compareTo(B) > 0 ? "Yes" : "No");
+        System.out.println(isAnagram("anagram", "margana"));
+        System.out.println(isAnagram("anagramm", "marganaa"));
+        System.out.println(isAnagram("Hello", "hello"));
+        System.out.println(isAnagram("act", "cat"));
+        System.out.println(isAnagram("stick", "ticks"));
 
     }
 
 //    ImplementedInterfaceNames method takes an object and prints the name of the interfaces it implemented
-    static void ImplementedInterfaceNames(Object o){
-        Class[] theInterfaces = o.getClass().getInterfaces();
-        for (int i = 0; i < theInterfaces.length; i++){
-            String interfaceName = theInterfaces[i].getName();
-            System.out.println(interfaceName);
-        }
-    }
+//    static void ImplementedInterfaceNames(Object o){
+//        Class[] theInterfaces = o.getClass().getInterfaces();
+//        for (int i = 0; i < theInterfaces.length; i++){
+//            String interfaceName = theInterfaces[i].getName();
+//            System.out.println(interfaceName);
+//        }
+//    }
 
     //capitalize the first letter of a string
-    private static String capitalizeString(String str) {
-        return Character.toUpperCase(str.charAt(0)) + str.substring(1);
+//    private static String capitalizeString(String str) {
+//        return Character.toUpperCase(str.charAt(0)) + str.substring(1);
+//    }
+
+    static boolean isAnagram(String a, String b) {
+// test for invalid input
+        if( a == null || b == null || a.equals("") || b.equals("") )
+            throw new IllegalArgumentException();
+
+// initial quick test for non-anagrams
+        if ( a.length() != b.length() )
+            return false;
+
+        a = a.toLowerCase();
+        b = b.toLowerCase();
+
+// populate a map with letters and frequencies of String b
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (int k = 0; k < b.length(); k++){
+            char letter = b.charAt(k);
+
+            if( ! map.containsKey(letter)){
+                map.put( letter, 1 );
+            } else {
+                Integer frequency = map.get( letter );
+                map.put( letter, ++frequency );
+            }
+        }
+
+// test each letter in String a against data in the map
+// return if letter is absent in the map or its  frequency is 0
+// otherwise decrease the frequency by 1
+
+        for (int k = 0; k < a.length(); k++){
+            char letter = a.charAt(k);
+
+            if( ! map.containsKey( letter ) )
+                return false;
+
+            Integer frequency = map.get( letter );
+
+            if( frequency == 0 )
+                return false;
+            else
+                map.put( letter, --frequency);
+        }
+// if the code got that far it is an anagram
+        return true;
     }
+
+
 }
