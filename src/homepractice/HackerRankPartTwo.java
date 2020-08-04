be5a13560a67c44ca5907b3f0da77a8456a7d7e6
Sarah L Nainggolan <sarah.l.nainggolan@gmail.com>
2020-08-04 09:17:14 -0500

    finish Collections.sort exercise to sort in descending order.

--- src/homepractice/HackerRankPartTwo.java
+++ src/homepractice/HackerRankPartTwo.java
@@ -1,41 +1,71 @@
 package homepractice;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class HackerRankPartTwo {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+//        Scanner sc = new Scanner(System.in);
 
 //        Maps names (keys) to phone numbers (values)
-        Map<String, Integer> phoneBook = new HashMap<>();
+//        Map<String, Integer> phoneBook = new HashMap<>();
+//
+//        System.out.println("How many contacts?");
+//        int numEntries = sc.nextInt();
+//        sc.nextLine();
+//        for (int i = 0; i < numEntries; i++) {
+//            System.out.println("Enter name:");
+//            String name = sc.nextLine();
+//
+//            System.out.println("Enter 8-digit phone#: ");
+//            int number = sc.nextInt();
+//            sc.nextLine();
+//            phoneBook.put(name, number);
+//        }
+//
+//        while (sc.hasNext()) {
+//            String name = sc.nextLine();
+//            Integer number = phoneBook.get(name);
+//
+//            if (phoneBook.containsKey(name)) {
+//                System.out.println(name + "=" + number);
+//            } else {
+//                System.out.println("Not found");
+//            }
+//        }
+//        sc.close();
 
-        System.out.println("How many contacts?");
-        int numEntries = sc.nextInt();
-        sc.nextLine();
-        for (int i = 0; i < numEntries; i++) {
+        //Collections.sort exercise to sort in descending cgpa order:
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter number of entries:");
+        int testCases = Integer.parseInt(in.nextLine());
+
+        List<Student> studentList = new ArrayList<Student>();
+        while(testCases>0){
+            System.out.println("Enter id:");
+            int id = in.nextInt();
+
             System.out.println("Enter name:");
-            String name = sc.nextLine();
+            String fname = in.next();
 
-            System.out.println("Enter 8-digit phone#: ");
-            int number = sc.nextInt();
-            sc.nextLine();
-            phoneBook.put(name, number);
+            System.out.println("Enter cgpa:");
+            double cgpa = in.nextDouble();
+
+            Student st = new Student(id, fname, cgpa);
+            studentList.add(st);
+
+            testCases--;
         }
 
-        while (sc.hasNext()) {
-            String name = sc.nextLine();
-            Integer number = phoneBook.get(name);
+        Collections.sort(studentList, Comparator.comparing(Student :: getCgpa).reversed().
+                thenComparing(Student :: getFname).thenComparing(Student :: getId));
 
-            if (phoneBook.containsKey(name)) {
-                System.out.println(name + "=" + number);
-            } else {
-                System.out.println("Not found");
-            }
+        for(Student st: studentList){
+            System.out.println(st.getFname());
         }
-        sc.close();
 
     }
+
+
+
 }
